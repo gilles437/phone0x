528df6cb406145be220e9a4d652632d37b42c73c
Gilles Barnier <gilles.barnier@gmail.com>
2022-11-03 13:08:04 -0600

    Update 2_deploy_contracts.js

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -7,5 +7,5 @@ const phone0x = artifacts.require("phone0x");
 console.log('process.env.ADDR1', process.env.ADDR1)
 module.exports = function(deployer) {
 
-  deployer.deploy(phone0x, process.env.ADDR1, '0x31302e31302e31302e3130000000000000000000000000000000000000000000');
+  deployer.deploy(phone0x, process.env.ADDR1,NODEIP1);
 };
